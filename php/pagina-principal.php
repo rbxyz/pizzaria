ec610759592036cbc6dd04f126cc99f9d7c25e2d
Ruan Gabriel <110505289+rbxyz@users.noreply.github.com>
2023-10-21 22:04:09 -0300

    Add files via upload

---
+++ php/pagina-principal.php
@@ -0,0 +1,48 @@
+<?
+include 'funcoes.php';
+$conexao = conectar();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Listar pessoas</title>
+</head>
+<body>
+    <a href="index.html">Voltar</a>
+    <br>
+    
+    <?php
+    $conexao = null;
+
+
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pizzaria dos guri</title>
+</head>
+<body>
+    <body>
+        <header>
+            <h1>Bem-vindo à Pizzaria dos gugu</h1>
+        </header>    
+        <div class="menu">
+            <h2>Biografia</h2>
+
+        </div>
+    
+        <footer>
+            <p>&copy; 2023 Pizzaria dos gugu. Todos os direitos reservados.</p>
+        </footer>
+    </body>
+</body>
+</html>
+
+
+
