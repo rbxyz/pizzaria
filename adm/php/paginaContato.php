73be4e943e2ad167cb427f01fbc34ffd2c709762
Ruan Gabriel <110505289+rbxyz@users.noreply.github.com>
2023-10-26 00:14:10 -0300

    Create paginaContato.php

---
+++ adm/php/paginaContato.php
@@ -0,0 +1,29 @@
+<?php
+include 'funcoes.php';
+$conexao = conectar();
+?>
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title> <-submeter mensagem-> </title>
+    </head>
+    <body>
+    <?php
+    $nome = $_POST['nome'];
+    $mensagem = $_POST['mensagem'];
+    $retornodafuncaoinserida = inserirMensagem($conexao, $nome, $mensagem);
+        $conexao = null;
+            if ($retornodafuncaoinserida == true) {
+                echo("Inserido os dados corretamente!");
+            }else{
+                echo("Não foi possivel inserir os dados.");
+            }
+    ?>
+    </body>
+    </html>
+
+    
+
+                
